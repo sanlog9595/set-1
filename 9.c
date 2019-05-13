320ccbbfc74c5f1d4815203ffcfde68f6a8d996f
sanlog9595 <49307730+sanlog9595@users.noreply.github.com>
2019-05-13 15:17:39 +0530

    9.c

---
+++ 9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	// your code goes here
+	int f,h,i,sum=0;
+	scanf("%d%d",&f,&h);
+	
+		for(f=1;f<=h;f++)
+		{
+			sum=sum+f;
+		
+		}
+		printf("%d",sum);
+	return 0;
+}
